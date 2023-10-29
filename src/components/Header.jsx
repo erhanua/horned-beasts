a591bb084dbc0ef35b2b7d01fa46daaa81ea2658
Adınız Soyadınız <email@adresiniz.com>
2023-10-29 18:20:58 +0000

    fuzzy searc was done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-function Header() {
+function Header({ setSearchInput }) {
   return (
     <header className="header">
       <h1>Gallery Title</h1>
+      <div className="form-input">
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </div>
     </header>
   );
 }
